f90d05ccf8f8ac497202994c27c0831881deb46f
BustyLoli-Chan <rjfenton@mtu.edu>
2015-02-25 14:46:28 -0500

    Implemented bare bones view "functionality"

--- STARmain/src/main.java
+++ STARmain/src/main.java
@@ -1,4 +1,22 @@
+import java.awt.GraphicsEnvironment;
+
 
 public class main {
-// herro, I am a placeholder!
+  // herro, I am a placeholder!
+  
+  //instantiate view class
+  static view mySearchView = new view();
+  
+  public static void main(String[] args){
+    
+    //BEGIN VIEW
+    //check for a windowed environment
+    if (!GraphicsEnvironment.isHeadless()) {
+      //add a tray item of sorts
+      
+      //TODO: add keyboard triggers
+      //show search window
+      mySearchView.setVisible(true);
+    }
+  }
 }
