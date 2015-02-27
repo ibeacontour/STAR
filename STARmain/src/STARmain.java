f01f7543987d5ba3fab43a9b0ec8d9a9b356743b
Tanner Ingersoll <tjingers@mtu.edu>
2015-02-27 15:49:40 -0500

    Renamed the 'main' file to 'STARmain'

--- STARmain/src/STARmain.java
+++ STARmain/src/STARmain.java
@@ -1,25 +1,26 @@
 import java.util.ArrayList;
 import java.awt.GraphicsEnvironment;
 
-public class main  {
+public class STARmain  {
 	static view mySearchView = new view();
 	public static void main(String[] args) throws InterruptedException  {
-		
+
 		// creates a model then does a search
 		Model model = new Model();
 		model.SearchFor("AA");
-		
-		//BEGIN VIEW
-    //check for a windowed environment
-    if (!GraphicsEnvironment.isHeadless()) {
-      //add a tray item of sorts
-      
-      //TODO: add keyboard triggers
-      //show search window
-      mySearchView.setVisible(true);
-	
 
+		//BEGIN VIEW
+		//check for a windowed environment
+		if (!GraphicsEnvironment.isHeadless()) {
+			//add a tray item of sorts
+
+			//TODO: add keyboard triggers
+			//show search window
+			mySearchView.setVisible(true);
+
+
+
+		}
 
 	}
-	
 }
