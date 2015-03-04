a52f02be8d3bdd1bef447b0216b5cdbc4a8ffd2b
ibeacontour <alvangil@mtu.edu>
2015-03-03 19:14:50 -0500

    Changed from using a custom thread to a custom runnable (easier to implement the way we want and safer to stop)

--- STARmain/src/Model.java
+++ STARmain/src/Model.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 
 public class Model {
 	String fileToFind;
-	MyThread searchThread = new MyThread();
 	ArrayList<String> results = new ArrayList<String>();
 	
 	// blank constructor
@@ -14,14 +13,21 @@ public class Model {
 
 	// function that runs the thread then prints the results it gets from the thread
 	public void SearchFor(String fileNameToFind) throws InterruptedException {
+		ArrayList<String> results2;
 		fileToFind = fileNameToFind;
-		searchThread.run(fileToFind);
+		MyRunnable run = new MyRunnable("AA");
+		Thread t1 = new Thread(run);
+		t1.start();
+		t1.sleep(1000);
 		
-		results = searchThread.getStuff();
+		
+		results = run.getStuff();
 		
 		for(int i = 0;i < results.size();i++) {
 			System.out.println(" " + results.get(i));
 		}
+		//System.out.println(" " + results.size());
+		//System.out.println(" " + results2.size());
 		
 	}
 }
--- STARmain/src/MyRunnable.java
+++ STARmain/src/MyRunnable.java
@@ -1,37 +1,40 @@
 import java.util.ArrayList;
 
-public class MyThread extends Thread{
-	boolean stop = false;
+public class MyRunnable implements Runnable{
+	boolean finished = false;
+	volatile boolean running = true;
+	String theFile;
 	ArrayList<String> stuff = new ArrayList<String>();
 	ArrayList<String> dummyDemo = new ArrayList<String>();
 	
-	public MyThread() {
+	public MyRunnable(String theFileToFind) {
 		// delete these when things start to work
 		// just a demo arraylist to show that something can work
+		theFile = theFileToFind;
 		dummyDemo.add("AAA");
 		dummyDemo.add("AA");
 		dummyDemo.add("A");
 	}
 
 	// main heavy lifting method that likes a string to look for
-	public void run(String theFile) {
+	//public void run(String theFile) {
+	public void run() {
+		System.out.println("I get here");
 		// when thread.stopThread is called, this while condition will break and end the thread process
-		while(stop == false) {
+		while(!Thread.currentThread().isInterrupted() && finished == false) {
 			// go through the dummy arraylist and compare strings it has with the string it was passed
 			for(int i = 0;i < dummyDemo.size();i++) {
-				if(theFile.compareTo(dummyDemo.get(i)) <= 0) {
+				// a dummy comparison
+				if(theFile.compareTo(dummyDemo.get(i)) >= 0) {
 					stuff.add(dummyDemo.get(i));
 				}
-			} stop = true;
+				
+			} finished = true;
 			
 		}
-		// print out that tells when it stops
-		System.out.println("I stopped");
+		
 	}
-	// safely stops the thread
-	public void stopThread() {
-		stop = true;
-	}
+
 	// getter function
 	public ArrayList<String> getStuff() {
 		return stuff;
